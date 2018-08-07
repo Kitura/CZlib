232570457b6c5b643d4af14eb8387f4f39bfcbb5
Kye Maloy <kye.maloy@ibm.com>
2018-08-07 13:38:44 +0100

    added new methods to wrap C code away from Swift compiler

--- shim.h
+++ shim.h
@@ -20,5 +20,20 @@
 
  #import <stdio.h>
  #import <zlib.h>
+
+static inline int CZlib_deflateInit2(z_streamp strm,
+                                           int level,
+                                           int method,
+                                           int windowBits,
+                                           int memLevel,
+                                           int strategy) {
+    return deflateInit2(strm, level, method, windowBits, memLevel, strategy);
+}
+
+static inline int CZlib_inflateInit2(z_streamp strm, int windowBits) {
+    return inflateInit2(strm, windowBits);
+}
+
+#endif
  
  #endif
